212410bc9a66d91d3ae536fbbcce019abf6669d8
Matthew Serre <matthew_serre+github@outlook.com>
2020-11-22 21:29:28 -0500

    Update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_025617) do
+ActiveRecord::Schema.define(version: 2020_06_28_163839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,8 +69,10 @@ ActiveRecord::Schema.define(version: 2020_06_20_025617) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "price"
+    t.bigint "repairable_id", null: false
     t.index ["order_id"], name: "index_order_items_on_order_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
+    t.index ["repairable_id"], name: "index_order_items_on_repairable_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -134,4 +136,5 @@ ActiveRecord::Schema.define(version: 2020_06_20_025617) do
   add_foreign_key "line_items", "products"
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
+  add_foreign_key "order_items", "repairables"
 end
