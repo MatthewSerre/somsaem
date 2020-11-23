a8a9a0de0479e1204e55b69ada431a6954be6bb3
Matthew Serre <matthew_serre+github@outlook.com>
2020-11-22 21:51:44 -0500

    Update serializer file

--- app/serializers/repairable_serializer.rb
+++ app/serializers/repairable_serializer.rb
@@ -1,4 +1,4 @@
-class RepairableSerializer < ActiveModel::Serializer
+class RepairableSerializer < ActiveModel::Serializers
     attributes :repair_name, :repair_price, :estimated_time
     belongs_to :product
 end
